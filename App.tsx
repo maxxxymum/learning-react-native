ab440a40a86b5c48f71901f399c9fb129eeff956
Max Martyn <maxmartyn@Maxs-MacBook-Pro.local>
2020-04-14 15:20:11 +0300

    feat: remove inline styles to StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{width: '80%', borderColor: "black", borderWidth: 1, padding: 10}} />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+        />
 
-        <Button title='ADD' />
+        <Button title="ADD" />
       </View>
 
       <View></View>
@@ -16,5 +19,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  }
 });
