a41d6a7387bd92b66fd6f606d4403a3716dd156b
Max Martyn <maxmartyn@Maxs-MacBook-Pro.local>
2020-04-14 15:32:43 +0300

    feat: Implement new goal adding

--- App.tsx
+++ App.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState<String>('');
+  const [courseGoals, setCourseGoals] = useState<String[]>([]);
+
+  function goalInputHandler(text: string): void {
+    setEnteredGoal(text);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course Goal"
           style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
 
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
 
       <View></View>
